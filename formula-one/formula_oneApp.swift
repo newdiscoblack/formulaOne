9f059d9e3577fc584cd897a4cbe02da996247229
newdiscoblack <kacperjagieo@gmail.com>
2021-04-08 18:44:13 +0200

    Initial Commit

---
+++ formula-one/formula_oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  formula_oneApp.swift
+//  formula-one
+//
+//  Created by Kacper Jagiello on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct formula_oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
